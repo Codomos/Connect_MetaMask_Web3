dc5f46cdc3f385991482b375b4ea67b699f32df5
Codomos <66867658+Codomos@users.noreply.github.com>
2023-04-16 14:58:00 +0000

    CSS styles added

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import './App.css';
 import { useState } from 'react';
 import { ethers } from 'ethers';
 
+// declare global {
+//   interface Window {
+//     ethereum: any;
+//   }
+// }
+
 function App() {
 
-  // Properties
-
   const [walletAddress, setWalletAddress] = useState("");
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
 
-  // Helper Functions
 
-  // Requests access to the user's META MASK WALLET
-  // https://metamask.io
-  async function requestAccount() {
+
+  async function ConnectMetamask() {
     console.log('Requesting account...');
 
     // ❌ Check if Meta Mask Extension exists 
-    if(window.ethereum) {
+    if (window.ethereum) {
       console.log('detected');
 
       try {
@@ -34,24 +38,52 @@ function App() {
   }
 
   // Create a provider to interact with a smart contract
-  async function connectWallet() {
-    if(typeof window.ethereum !== 'undefined') {
-      await requestAccount();
+  async function Transfer() {
+    if (typeof window.ethereum !== 'undefined') {
+      await ConnectMetamask();
+      const tx = {
+        from: walletAddress,
+        to: input2,
+        value: ethers.utils.parseUnits(input1, "ether").toHexString()
+      }
 
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await window.ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [tx],
+      }, (error, result) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(result);
+        }
+      });
+
+
+      // const provider = new ethers.providers.Web3Provider(window.ethereum);
     }
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button
-        
-        onClick={requestAccount}
-        
-        >Request Account</button>
-        <h3>Wallet Address: {walletAddress}</h3>
-      </header>
+      <button onClick={ConnectMetamask}>Connect MetaMask</button>
+      <h3>Wallet Address: {walletAddress}</h3>
+      <div>
+        <input
+          type="text"
+          value={input1}
+          onChange={(e) => setInput1(e.target.value)}
+          placeholder="Enter Value in ETH"
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={input2}
+          onChange={(e) => setInput2(e.target.value)}
+          placeholder="Enter Receiver Address"
+        />
+      </div>{' '}
+      <button onClick={Transfer}>Tranasfer</button>
     </div>
   );
 }
